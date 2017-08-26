d3f03c7671035dd313ce420f341ff7d36583f5b1
Chown <sunderphon@gmail.com>
2017-08-26 09:17:57 +0100

    Added map generation and projectile

--- SHMUP/Assets/MapGenerator.cs
+++ SHMUP/Assets/MapGenerator.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
-	private GameObject[,] map = new GameObject[20, 300];
-	
+	public GameObject blankFloor;
+	public GameObject StationaryShooter;
 
+	public static int width = 6;
+	public static int height = 75;
 
+	private GameObject[,] map = new GameObject[width, height];
+
+	void Start() {
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				GameObject objInstance = Instantiate(blankFloor, new Vector3(x * 20, 0, y * 20), Quaternion.identity, transform);
+				map[x, y] = objInstance;
+			}
+		}
+	}
 }
