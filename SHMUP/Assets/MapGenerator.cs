b55e8de8d0d2e865a079d4becab8c5c283d862e1
Chown <sunderphon@gmail.com>
2017-08-26 04:30:22 +0100

    Added space resources

---
+++ SHMUP/Assets/MapGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+	private GameObject[,] map = new GameObject[20, 300];
+	
+
+
+}
